c5524dd105a54d2bb087a55e0607c500318b3cf1
Antonio <reytono@gmail.com>
2018-05-12 13:01:32 -0600

    nueva scene

---
+++ Assets/Scripts/TimerMundo2.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimerMundo2 : MonoBehaviour
+{
+    public string levelToLoad;
+    public int timeLeft = 100;
+    public Text countdownText;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("LoseTime");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdownText.text = ("" + timeLeft);
+
+        if (timeLeft <= 0)
+        {
+            SceneManager.LoadScene("Mundo2PLAY", LoadSceneMode.Single);
+        }
+
+        if (GameController.instance.gameOver == true)
+        {
+            countdownText.text = "";
+        }
+    }
+
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+    }
+}

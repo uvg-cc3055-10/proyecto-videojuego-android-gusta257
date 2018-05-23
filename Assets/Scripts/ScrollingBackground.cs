11d1d2b61ea0c585b98cde8e6617c768ea1b1053
Gustavo de Leon <gdeleontobias@gmail.com>
2018-05-22 18:23:20 -0600

    TERMINAR DIFICIL

--- Assets/Scripts/ScrollingBackground.cs
+++ Assets/Scripts/ScrollingBackground.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ScrollingBackground : MonoBehaviour {
 
-    float scrollingSpeed = 8f;
+     float scrollingSpeed = 8f;
 
 	// Use this for initialization
 	void Start () {
